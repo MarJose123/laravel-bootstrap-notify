c5c8c03e45641c8f4f56122d78e756dd05877834
Josie Darang <josie.darang@devtac.ph>
2021-10-19 10:23:49 +0800

    added the alert blade

---
+++ resources/views/components/notify.blade.php
@@ -0,0 +1,19 @@
+@if (session()->has('notify.message'))
+
+    @include('notify::notifications.danger')
+
+    @include('notify::notifications.info')
+
+    @include('notify::notifications.success')
+
+    @include('notify::notifications.warning')
+
+@endif
+
+{{ session()->forget('notify.message') }}
+
+<script>
+    var notify = {
+        timeout: "{{ config('notify.timeout') }}",
+    }
+</script>
