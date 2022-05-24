6b8baa63ac261f3c4377eb2c5f57a61fa2263c8f
Josie Darang <josie.darang@devtac.ph>
2022-05-24 15:13:12 +0800

    added preset toast

--- resources/js/notify.core.js
+++ resources/js/notify.core.js
@@ -227,7 +227,55 @@
                         '<button type="button" aria-hidden="true" class="close" data-notify="dismiss">×</button>' +
                         '<span data-notify="icon">{0}</span> ' +
                         '<span data-notify="title">{1}</span> ' +
-                        '<span data-notify="message">{2}</span>' +
+                        '<spasn data-notify="message">{2}</spasn>' +
+                        '<div class="progress" data-notify="progressbar">' +
+                        '<div class="progress-bar progress-bar-{0}" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width: 0%;"></div>' +
+                        '</div>' +
+                        '<a href="{3}" target="{4}" data-notify="url"></a>' +
+                        '</div>'
+                });
+
+            },
+            preset: (preset_name) => {
+
+                $.notify({
+                    // options
+                    icon: preset_name.icon,
+                    title: preset_name.items,
+                    message:preset_name.message,
+                },{
+                    // settings
+                    element: 'body',
+                    position: null,
+                    type: "success",
+                    allow_dismiss: true,
+                    newest_on_top: true,
+                    showProgressbar: false,
+                    placement: {
+                        from: "top",
+                        align: "right"
+                    },
+                    offset: 20,
+                    spacing: 10,
+                    z_index: 1031,
+                    delay: 5000,
+                    timer: 1000,
+                    url_target: '_blank',
+                    mouse_over: null,
+                    animate: {
+                        enter: 'animated fadeInDown',
+                        exit: 'animated fadeOutUp'
+                    },
+                    onShow: null,
+                    onShown: null,
+                    onClose: null,
+                    onClosed: null,
+                    icon_type: 'class',
+                    template: '<div data-notify="container" class="col-xs-11 col-sm-3 alert alert-{0[type]}" role="alert">' +
+                        '<button type="button" aria-hidden="true" class="close" data-notify="dismiss">×</button>' +
+                        '<span data-notify="icon">{preset_name.icon}</span> ' +
+                        '<span data-notify="title">{preset_name.items}</span> ' +
+                        '<spasn data-notify="message">{preset_name.message}</spasn>' +
                         '<div class="progress" data-notify="progressbar">' +
                         '<div class="progress-bar progress-bar-{0}" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width: 0%;"></div>' +
                         '</div>' +
